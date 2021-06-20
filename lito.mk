96a5110ad52c1f8ac0ce1bb953b63de856188d72
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-06-20 02:26:17 +0200

    sm7250-common: Stop building vendor.qti.hardware.perf lib
    * We dropped QTI perf stack
    
    Change-Id: Ie71344aa05888adb7ad76ad66693547fc50a0025

--- lito.mk
+++ lito.mk
@@ -296,10 +296,6 @@ PRODUCT_PACKAGES += \
     libOmxVenc \
     libstagefrighthw
 
-# Perf
-PRODUCT_PACKAGES += \
-    vendor.qti.hardware.perf@2.2.vendor
-
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service.xiaomi-libperfmgr
