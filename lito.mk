cedd2fecf6ab25be4d06300ef71962cc862779b1
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-10-14 19:15:50 +0200

    sm7250-common: Move to AOSP NXP NFC HAL
    * NXP added snxxx support in AOSP, \o/
    
    Change-Id: Id1b9cdbe1f29d1ff61af8182ae2f8370e5210426

--- lito.mk
+++ lito.mk
@@ -312,17 +312,13 @@ PRODUCT_PACKAGES += \
 
 # NFC
 PRODUCT_PACKAGES += \
-    android.hardware.secure_element@1.2.vendor \
+    android.hardware.nfc_snxxx@1.2-service \
+    android.hardware.secure_element_snxxx@1.2-service \
     com.android.nfc_extras \
-    com.gsma.services.nfc \
     libchrome.vendor \
     NfcNci \
     SecureElement \
-    Tag \
-    vendor.nxp.hardware.nfc@1.1 \
-    vendor.nxp.hardware.nfc@2.0 \
-    vendor.nxp.hardware.nfc@1.1.vendor \
-    vendor.nxp.hardware.nfc@2.0.vendor
+    Tag
 
 # OMX
 PRODUCT_PACKAGES += \
