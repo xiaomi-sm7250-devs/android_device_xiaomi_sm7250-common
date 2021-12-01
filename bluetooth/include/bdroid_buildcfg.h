35b50df64f3224fe123f87549a5c4a46327b094e
Erfan Abdi <erfangplus@gmail.com>
2021-12-01 16:42:55 +0100

    sm7250-common: Commonize bluetooth header

---
+++ bluetooth/include/bdroid_buildcfg.h
@@ -0,0 +1,61 @@
+/*
+ *
+ *  Copyright (c) 2013, The Linux Foundation. All rights reserved.
+ *  Not a Contribution, Apache license notifications and license are retained
+ *  for attribution purposes only.
+ *
+ * Copyright (C) 2012 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef _BDROID_BUILDCFG_H
+#define _BDROID_BUILDCFG_H
+
+#include <stdint.h>
+#include <string.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+int property_get(const char *key, char *value, const char *default_value);
+#ifdef __cplusplus
+}
+#endif
+
+static inline const char* BtmGetDefaultName()
+{
+    char product_device[92];
+    property_get("ro.product.device", product_device, "");
+
+    if (strstr(product_device, "monet"))
+        return "Mi 10 Lite 5G";
+    if (strstr(product_device, "vangogh"))
+        return "Mi 10 Lite Zoom";
+    if (strstr(product_device, "picasso"))
+        return "Redmi K30 5G";
+
+    // Fallback to Xiaomi Generic
+    return "Xiaomi";
+}
+
+#define BTM_DEF_LOCAL_NAME BtmGetDefaultName()
+// Disables read remote device feature
+#define BTM_WBS_INCLUDED TRUE
+#define BTIF_HF_WBS_PREFERRED TRUE
+
+#define BLE_VND_INCLUDED   TRUE
+// skips conn update at conn completion
+#define BT_CLEAN_TURN_ON_DISABLED 1
+
+#endif
