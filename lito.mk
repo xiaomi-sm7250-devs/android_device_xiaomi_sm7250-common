f52759c8e15a35b5d76ca59eacd273131a43afc9
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-05-09 01:34:54 +0200

    sm7250-common: Implement UDFPS handler
    Change-Id: I7afff305140639d38ebb8bc51fbe790a1636db9e

--- lito.mk
+++ lito.mk
@@ -206,6 +206,9 @@ PRODUCT_PACKAGES += \
 
 ifeq ($(TARGET_HAS_FOD),true)
 PRODUCT_PACKAGES += \
+    libudfpshandler
+
+PRODUCT_PACKAGES += \
     vendor.goodix.hardware.biometrics.fingerprint@2.1.vendor
 endif
 
